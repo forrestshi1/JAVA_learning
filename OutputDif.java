6b1ace6aee8d9dcb039dd40558662a2af845e2fb
forrestshi <forrestshi802@gmail.com>
2023-04-04 23:59:13 +0800

    learn how to compile in command and know the difference of println and print

---
+++ OutputDif.java
@@ -0,0 +1,9 @@
+public class OutputDif  {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");// output everything on the same line
+    System.out.println("Ready or not, here I come!");
+  }
+}
---
+++ comment.java
@@ -0,0 +1,13 @@
+public class comment {
+  public static void main(String[] args){
+	    System.out.println("Hello I'M Forrest!");
+	    System.out.println("i am born in 2002");
+	    //Sun Microsystems announced the release of Java in 1995
+	    
+	/*James Gosling is a Canadian engineer who 
+			created Java while working at Sun Microsystems. 
+			His favorite number is the square root of 2!
+	    */
+	    System.out.println("JAVA is  a fun language!");
+	  }
+	} 
