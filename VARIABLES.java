1e82b1178ed9ccdac91c5fcf2b851ee3a9f31013
forrestshi <forrestshi802@gmail.com>
2023-04-06 00:27:37 +0800

    learn the variables

---
+++ VARIABLES.java
@@ -0,0 +1,36 @@
+public class VARIABLES {
+	public static void main(String[] args) {   
+    String name = "forest";
+    int age = 20;
+    double desiredSalary = 5.1;
+    char gender = 'm';
+    boolean lookingForJob = true;
+    
+    
+    
+    
+     /*
+  This program genrates a mad libbed story.
+  Author:Forrest
+  Date:05/04/2023
+  */
+      String name1 = "Tim";
+      String name2 = "Dona";
+      String adjective1 = "happy";
+      String adjective2 = "nice";
+      String adjective3 = "cold";
+      String verb1 = "go";
+      String noun1 = "sky";
+      String noun2 = "flower";
+      String noun3 = "car";
+      String noun4 = "cat";
+      String noun5 = "dog";
+      String noun6 = "water";
+      int number = 2;
+      String place1 = "Tokyo";
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+      System.out.println(story);
+	}
+}
+// know the type of the variables
