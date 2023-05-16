5a7f15e2b0dc062cc6370e235af8dd2859f20d46
forrestshi <forrestshi802@gmail.com>
2023-05-17 00:08:55 +0800

    Thread Daemon 守护线程

---
+++ ThreadDaemon.java
@@ -0,0 +1,31 @@
+package default2;
+//守护线程一般为工作线程服务，当所有用户线程结束，自动结束
+
+public class ThreadDaemon {
+    public static void main(String[] args) throws InterruptedException {
+        MyDaemonThread myDaemonThread = new MyDaemonThread();
+        //如果我们希望当main线程结束后，子线程自动结束
+        //,只需将子线程设为守护线程即可
+        myDaemonThread.setDaemon(true);//设置为守护线程
+        myDaemonThread.start();
+
+        for( int i = 1; i <= 10; i++) {//main线程
+            System.out.println("宝强在辛苦的工作...");
+            Thread.sleep(1000);
+        }
+    }
+}
+
+class MyDaemonThread extends Thread {
+    public void run() {
+        for (; ; ) {//无限循环
+            try {
+                Thread.sleep(1000);//休眠1000毫秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("马蓉和宋喆快乐聊天，哈哈哈~~~");
+        }
+    }
+}
+
