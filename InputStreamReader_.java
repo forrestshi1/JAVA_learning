5479bedfbfd65bd9d3fb7961f558b442b727abfa
forrestshi <forrestshi802@gmail.com>
2023-06-17 22:26:22 +0800

    字符字节转换流transformation

---
+++ InputStreamReader_.java
@@ -0,0 +1,34 @@
+package default2;
+
+import java.io.*;
+
+/**
+
+ * 演示使用 InputStreamReader 转换流解决中文乱码问题
+ * 将字节流 FileInputStream 转成字符流  InputStreamReader, 指定编码 gbk/utf-8
+ */
+public class InputStreamReader_ {
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "d:\\test.txt";
+        //解读
+        //1. 把 FileInputStream 转成 InputStreamReader
+        //2. 指定编码 gbk
+        //InputStreamReader isr = new InputStreamReader(new FileInputStream(filePath), "gbk");
+        //3. 把 InputStreamReader 传入 BufferedReader
+        //BufferedReader br = new BufferedReader(isr); //字符流到包装流
+
+        //将2 和 3 合在一起
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                                                    new FileInputStream(filePath), "gbk"));
+
+        //4. 读取
+        String s = br.readLine();
+        System.out.println("读取内容=" + s);
+        //5. 关闭外层流
+        br.close();
+
+    }
+
+
+}
---
+++ OutputStreamWriter_.java
@@ -0,0 +1,21 @@
+package default2;
+
+import java.io.*;
+
+/**
+ * 演示 OutputStreamWriter 使用
+ * 把FileOutputStream 字节流，转成字符流 OutputStreamWriter
+ * 指定处理的编码 gbk/utf-8/utf8
+ */
+public class OutputStreamWriter_ {
+    public static void main(String[] args) throws IOException {
+        String filePath = "d:\\test.txt";
+        String charSet = "utf-8";
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(filePath), charSet);
+        osw.write("hi, 你好~");
+        osw.close();
+        System.out.println("按照 " + charSet + " 保存文件成功~");
+
+
+    }
+}
