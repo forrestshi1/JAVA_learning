dfb8d7f35097605c643a0b545092cf056688ce45
forrestshi1 <129770412+forrestshi1@users.noreply.github.com>
2023-07-17 21:28:43 +0800

    Add files via upload
    DAO

---
+++ dao_/dao/ActorDAO.java
@@ -0,0 +1,12 @@
+package dao_.dao;
+
+import dao_.domain.Actor;
+
+/**
+ * @author 韩顺平
+ * @version 1.0
+ */
+public class ActorDAO extends BasicDAO<Actor> {
+    //1. 就有 BasicDAO 的方法
+    //2. 根据业务需求，可以编写特有的方法.
+}
