7c45054f77fd3f78d639d252c0ce3bf974f7ed15
forrestshi1 <129770412+forrestshi1@users.noreply.github.com>
2024-02-20 20:59:37 +0800

    Delete demo directory

--- demo/src/main/java/com/example/demo/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import com.example.demo.pojp.User;
-
-public class UserService {
-    public void printUser(User user) {
-        System.out.println("user");
-    }
-}
